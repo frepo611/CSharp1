607a5dbfd6e68aeccf4b5fc447c29b378b9dab25
fredr <fredr@VISHNU>
2024-09-18 08:47:26 +0200

    Add project files.

---
+++ howoldareyou/Program.cs
@@ -0,0 +1,22 @@
+﻿DateOnly userBirthdate;
+while (true)
+{
+    Console.WriteLine("What is your birthdate");
+    string userInput = Console.ReadLine();
+    bool validBirthdate = DateOnly.TryParse(userInput, out userBirthdate);
+    if (validBirthdate) break;
+}
+Console.WriteLine($"Your year of birth is {userBirthdate.Year}");
+Console.WriteLine($"You are {HowOld(userBirthdate)} years old");
+
+int HowOld(DateOnly userBirthdate)
+{   DateTime date = new DateTime(userBirthdate.Year,userBirthdate.Month,userBirthdate.Day);
+    if (DateTime.Compare(DateTime.Today, date) < 0)
+        {
+        throw new ArgumentException("The date cannot be in the future.");
+        }
+    return (DateTime.Today.Year - userBirthdate.Year);
+}
+
+Console.WriteLine("Press Enter to continue");
+Console.ReadLine();
---
+++ sagan/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace sagan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 0;
+            string name = "";
+            string species = "";
+            int noOfLegs = 0;
+            while (true)
+            {
+                Console.WriteLine("Ange en ålder:");
+                string inputAge = Console.ReadLine();
+                try
+                {
+                    age = int.Parse(inputAge);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Ange ett heltal, tack!");
+                    continue;
+                }
+                break;
+            }
+
+            Console.WriteLine("Ange ett namn:");
+            name = Console.ReadLine();
+            
+            Console.WriteLine("Ange en djursort:");
+            species = Console.ReadLine();
+
+            while (true)
+            {
+                Console.WriteLine($"Hur många ben har en {species}?");
+                string inputSpecies = Console.ReadLine();
+                try
+                {
+                    noOfLegs = int.Parse(inputSpecies);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Ange ett heltal, tack!");
+                    continue;
+                }
+                break;
+            }
+
+                string sagan = $"Det var en gång en {age} år gammal {species} som hette {name}. En dag var {name} ute på en\r\npromenad i skogen, och mötte en stor varg. Vargen bet av ett ben. {name} sprang snabbt hem på sina\r\n{noOfLegs - 1}. Så var sagan slut.\r\n";
+                Console.WriteLine(sagan);
+                Console.WriteLine("Press Enter to exit");
+                Console.ReadLine();
+        }
+    }
+}
