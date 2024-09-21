c0be916c4cefa9b8af92816ab0a11bc78e00f281
Fredrik von Post <fredrik.von.post@campusnykoping.se>
2024-09-21 23:00:05 +0200

    Full functionality

--- Labyrinten/Program.cs
+++ Labyrinten/Program.cs
@@ -1,27 +1,61 @@
-﻿int boardSize = 4;
+﻿
 
-// Draw upper border
-for (int i = 0; i < boardSize; i++)
+int boardSize = 3;
+
+bool drawAgain = true;
+do
 {
+    Console.WriteLine(
+    "Welcome to the grid.\nPress 'x' to increase the size\nPress 'z' to decrease the size\nPress 'q' to quit");
+    Console.WriteLine();
 
+    DrawGrid(boardSize);
+
+    ConsoleKeyInfo pressedKey = Console.ReadKey();
+    switch (pressedKey.KeyChar)
+    {
+        case 'x':
+        case 'X':
+            boardSize++;
+            break;
+        case 'z':
+        case 'Z':
+            boardSize--;
+            break;
+        default:
+            break;
+    }
+    if (pressedKey.KeyChar == 'q' || pressedKey.KeyChar == 'Q')
+        drawAgain = false;
+
+    Console.Clear();
+} while (drawAgain);
+
+Console.WriteLine("Goodbye. Press Enter.");
+Console.ReadLine();
+
+static void DrawGrid(int boardSize)
+{
+    for (int i = 0; i < boardSize; i++)
+    {
+
+        for (int j = 0; j <= 4 * boardSize; j++)
+        {
+            Console.Write('x');
+        }
+        Console.WriteLine();
+
+        for (int k = 0; k < 3; k++)
+        {
+            for (int l = 0; l <= boardSize; l++)
+            {
+                Console.Write("x   ");
+            }
+            Console.WriteLine();
+        }
+    }
     for (int j = 0; j <= 4 * boardSize; j++)
     {
         Console.Write('x');
     }
-    Console.WriteLine();
-
-    for (int k = 0; k < 3; k++)
-    {
-        for (int l = 0; l <= boardSize; l++)
-        {
-            Console.Write("x   ");
-        }
-        Console.WriteLine();
-    }
 }
-for (int j = 0; j <= 4 * boardSize; j++)
-{
-    Console.Write('x');
-}
-Console.ReadLine();
-
