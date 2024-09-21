18d5a9538232044db22eb64f661bba097ed339a8
Fredrik von Post <fredrik.von.post@campusnykoping.se>
2024-09-21 22:14:52 +0200

    Initial commit of Labyrinten. Drawing the grid using nested for loops.

---
+++ Labyrinten/Program.cs
@@ -0,0 +1,27 @@
+﻿int boardSize = 4;
+
+// Draw upper border
+for (int i = 0; i < boardSize; i++)
+{
+
+    for (int j = 0; j <= 4 * boardSize; j++)
+    {
+        Console.Write('x');
+    }
+    Console.WriteLine();
+
+    for (int k = 0; k < 3; k++)
+    {
+        for (int l = 0; l <= boardSize; l++)
+        {
+            Console.Write("x   ");
+        }
+        Console.WriteLine();
+    }
+}
+for (int j = 0; j <= 4 * boardSize; j++)
+{
+    Console.Write('x');
+}
+Console.ReadLine();
+
