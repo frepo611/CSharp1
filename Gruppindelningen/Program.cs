cf2bdfd4446935657b752f270c3cf0ca063b0f0f
Fredrik von Post <fredrik.von.post@campusnykoping.se>
2024-10-01 14:41:11 +0200

    Gruppindelningen: first commit, initialized array with names

---
+++ Gruppindelningen/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Gruppindelningen
+{
+    internal class Program
+    {
+        static readonly string[] _names = { "Karin", "Anders", "Johan", "Eva", "Maria", "Mikael", "Anna", "Sara",
+                           "Erik", "Per", "Christina", "Lena", "Lars", "Emma", "Kerstin", "Karl", "Marie" };
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
