970cb60dfb9b56f189a15310448505676e24b45d
Fredrik von Post <fredrik.von.post@campusnykoping.se>
2024-10-03 10:16:06 +0200

    refactored to a method

--- RäknaDiagonalen/Program.cs
+++ RäknaDiagonalen/Program.cs
@@ -12,21 +12,27 @@
                 { 21, 25, 71, 69, 39 }, // Fourth row
                 { 19, 39, 58, 28, 83 } // Fifth row
             };
-            int mainDiagonalSum = 0;
 
-            for (int i = 0; i < numbers.GetLength(0); i++)
-            {
-                for (int j = 0; j < numbers.GetLength(1); j++)
-                {
-                    if (i == j)
-                    {
-                        mainDiagonalSum += numbers[i, j];
-                    }
-                }
-            }
-            Console.WriteLine($"Diagonalsumman är: {mainDiagonalSum}");
+            Console.WriteLine($"Diagonalsumman är: {GetMainDiagonalSum(numbers)}");
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
+
+            static int GetMainDiagonalSum(int[,] matrix)
+            {
+                int mainDiagonalSum = 0;
+
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        if (i == j)
+                        {
+                            mainDiagonalSum += matrix[i, j];
+                        }
+                    }
+                }
+                return mainDiagonalSum;
+            }
         }
     }
 }
