ea4048bfe825eae0ac09fb4c14e646206b6172f9
Fredrik von Post <fredrik.von.post@campusnykoping.se>
2024-10-03 12:51:33 +0200

    RäknaDiagonalen: Added methods to write a matrix, and create a random matrix.

--- RäknaDiagonalen/Program.cs
+++ RäknaDiagonalen/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace RäknaDiagonalen
+﻿using System.Text;
+
+namespace RäknaDiagonalen
 {
     internal class Program
     {
@@ -13,9 +15,48 @@
                 { 19, 39, 58, 28, 83 } // Fifth row
             };
 
+            Console.WriteLine("Hårdkodad matris:");
+            Console.Write(MatrixToString(numbers));
             Console.WriteLine($"Diagonalsumman är: {GetMainDiagonalSum(numbers)}");
+            Console.WriteLine();
+
+            Console.WriteLine("Slumpad matris:");
+            int matrixSize = 6;
+            int[,] otherNumbers = GetRandomMatrix(matrixSize, 0, 100);
+            Console.Write(MatrixToString(otherNumbers));
+            Console.WriteLine($"Diagonalsumman är: {GetMainDiagonalSum(otherNumbers)}");
+
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
+
+            static string MatrixToString(int[,] array)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        sb.Append($"{array[i, j],-4}");
+                    }
+                    sb.AppendLine(); // New line for each row
+                }
+
+                return sb.ToString();
+            }
+
+            static int[,] GetRandomMatrix(int size, int minLimit, int maxLimit)
+            {
+                int[,] matrix = new int[size, size];
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix.GetLength(0); j++)
+                    {
+                        matrix[i, j] = Random.Shared.Next(minLimit, maxLimit);
+                    }
+                }
+                return matrix;
+            }
 
             static int GetMainDiagonalSum(int[,] matrix)
             {
@@ -34,5 +75,6 @@
                 return mainDiagonalSum;
             }
         }
+
     }
 }
