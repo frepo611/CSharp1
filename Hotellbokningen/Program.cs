7d8b824bc6ac7ab827213cc7f6d220b43470597e
Fredrik von Post <fredrik.von.post@campusnykoping.se>
2024-10-04 12:35:28 +0200

    gid damn ChatGPT found the indexing bug in the vacantIndex

--- Hotellbokningen/Program.cs
+++ Hotellbokningen/Program.cs
@@ -1,21 +1,70 @@
 ﻿
 
 
+
+
 namespace Hotellbokningen
 {
     internal class Program
     {
         static string[,] hotelRooms = new string[3, 4];
+        static int[] priceList = {650,775,995};
+        static int[] rooms = { 0, 1, 2, 3, 10, 11, 12, 13, 20, 21, 22, 23 };
         static void Main(string[] args)
         {
             int room = 10;
             string name = "Andersson";
-            PrintRooms();
-            Console.ReadKey();
+            //PrintRooms();
+            //Console.ReadKey();
             BookRoom(room, name);
+            BookRoom(12, "Jappo");
             PrintRooms();
+            //Console.ReadKey();
+            int nightlyRevenue = GetNightlyRevenue();
+            int[] vacantRooms = GetVacantRooms();
+            foreach (var item in vacantRooms)
+            {
+                Console.WriteLine(item);
+            }
             Console.ReadKey();
 
+        }
+
+        private static int[] GetVacantRooms()
+        {
+            //calculate how many vacant rooms
+            //create an int[]
+            //populate the array with vacant room numbers
+            int noOfVacants = 0;
+            for (int i = 0; i < rooms.Length; i++)
+            {
+                if (IsVacant(rooms[i])) noOfVacants++;
+            }
+            int[] vacantRooms = new int[noOfVacants];
+            
+            int vacantIndex = 0;
+            for (int i = 0; i < rooms.Length; i++)  // Should iterate through all rooms
+            {
+                if (IsVacant(rooms[i]))
+                {
+                    vacantRooms[vacantIndex] = rooms[i]; // Populate using correct index
+                    vacantIndex++; // Increment index only when a vacant room is found
+                }
+            }
+            return vacantRooms;
+        }
+
+        private static int GetNightlyRevenue()
+        {
+            int sum = 0;
+            for (int i = 0; i < hotelRooms.GetLength(0); i++)
+                {
+                    for (int j = 0; j < hotelRooms.GetLength(1); j++)
+                    {
+                        if (hotelRooms[i, j] != null) sum += priceList[i];
+                    }
+                }
+            return sum;
         }
 
         private static void BookRoom(int room, string name)
@@ -44,7 +93,14 @@ namespace Hotellbokningen
                     else
                         Console.Write($"{hotelRooms[i,j],-12}");
                 }
+                // print prices
                 Console.WriteLine();
+                for (int j = 0; j < hotelRooms.GetLength(1); j++)
+                    {
+                    string priceString = $"{priceList[i]} kr/natt";
+                    Console.Write($"{priceString,-15}");
+                    }
+                    Console.WriteLine("\n");
             }
         }
     }
