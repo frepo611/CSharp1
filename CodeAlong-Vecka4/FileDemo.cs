e7f2b50047dc7451f0e8fa1bfc188cc4986471ab
Fredrik von Post <fredrik.von.post@campusnykoping.se>
2024-10-07 11:38:05 +0200

    FileDemo: Topplistan

---
+++ CodeAlong-Vecka4/FileDemo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+namespace CodeAlong_Vecka4
+{
+    internal class FileDemo
+    {
+        public static void Run()
+        {
+            string[] mintop10;
+
+            if (File.Exists("top10.txt"))
+            {
+                mintop10 = File.ReadAllLines("top10.txt");
+            }
+            else
+            {
+                mintop10 = new string[10];
+            }
+
+            while (true)
+            {
+                Console.Clear();
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine($"{i + 1}: {mintop10[i]}");
+                }
+                Console.Write("Topplisteplats? ");
+                string input = Console.ReadLine();
+                bool validEntry;
+                validEntry = int.TryParse(input, out int postIndex);
+                if (validEntry)
+                {
+                    if ((postIndex - 1)>= 0 && (postIndex - 1) < mintop10.Length)
+                    {
+                        Console.Write($"Ny grej på plats nummer {postIndex}: ");
+                        mintop10[postIndex - 1] = Console.ReadLine();
+                    }
+                }
+
+                File.WriteAllLines("top10.txt", mintop10);
+            }
+        }
+    }
+}
+
---
+++ CodeAlong-Vecka4/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace CodeAlong_Vecka4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileDemo.Run();
+        }
+    }
+    internal class TernaryDemo
+        {
+        public static void Demo()
+        {
+            int x = 15;
+            int y = 5;
+
+            string result = (x > y) ? "x är större än y" : "y är mindre eller lika med x";
+            Console.WriteLine(result);
+
+            Console.Write("Vad heter du? ");
+            string name = Console.ReadLine();
+
+            string result2 = $"{((name == "Fredrik") ? "Hej" : "Stick!")}";
+            Console.WriteLine(result2);
+
+
+            Console.ReadKey();
+        }
+    }
+}
