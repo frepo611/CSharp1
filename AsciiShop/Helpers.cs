3c071365e57fba8507c8ddb702656102dda3157c
Fredrik von Post <fredrik.von.post@campusnykoping.se>
2024-10-13 17:22:11 +0200

    AsciiShop: first commit from Mickes code

---
+++ AsciiShop/Helpers.cs
@@ -0,0 +1,55 @@
+﻿namespace AsciiShop;
+
+internal class Helpers
+{
+    internal static void Draw(string[,] drawing, int[] cursor)
+    {
+        string frame = "#";
+
+        for (int top = 0; top < drawing.GetLength(1) + 2; top++)
+        {
+            Console.Write(frame);
+        }
+        Console.WriteLine();
+
+
+        for (int row = 0; row < drawing.GetLength(0); row++)
+        {
+            Console.Write(frame);
+            for (int col = 0; col < drawing.GetLength(1); col++)
+            {
+                if (row == cursor[0] && col == cursor[1])
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write(drawing[row, col] == null ? "_" : drawing[row, col]);
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    Console.Write(drawing[row, col] == null ? " " : drawing[row, col]);
+                }
+
+            }
+            Console.Write(frame);
+            Console.WriteLine();
+
+
+        }
+
+        for (int bottom = 0; bottom < drawing.GetLength(1) + 2; bottom++)
+        {
+            Console.Write(frame);
+        }
+        Console.WriteLine();
+    }
+    internal static void FileSave(string[,] drawing, string fileName)
+    {
+        // Övning Skapa filsparning
+
+    }
+    internal static string[,] FileLoad(string fileName)
+    {
+        // Övning: Skapa laddningsfunktion
+        return null;
+    }
+}
---
+++ AsciiShop/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace AsciiShop;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string[,] drawing = new string[10, 15];
+        // drawing[3, 3] = "Z";
+        int[] cursor = new int[2];
+        while (true)
+        {
+            Console.Clear();
+            Helpers.Draw(drawing, cursor);
+
+            ConsoleKeyInfo key = Console.ReadKey();
+
+            switch (key.KeyChar)
+            {
+                //case 'x': size++; break;
+                case 'e':
+                    Console.Write("Ange tecken: ");
+                    drawing[cursor[0], cursor[1]] = Console.ReadLine();
+                    break;
+                case 'a': cursor[1]--; break;
+                case 'd': cursor[1]++; break;
+                case 'w': cursor[0]--; break;
+                case 's': cursor[0]++; break;
+                case 'f':
+                    Console.Write("Filnamn: ");
+                    string filename = Console.ReadLine();
+                    Helpers.FileSave(drawing, filename);
+                    break;
+                //case 'l':
+                //    Console.Write("Filnamn: ");
+                //    string filename = Console.ReadLine();
+                //    Helpers.FileLoad(drawing, filename);
+                //    break;
+
+            }
+
+
+
+            // cursor = Helpers.ReadKeyboard(cursor);
+        }
+    }
+
+
+}
