2102d9af52ee5c37c9d617d45e8dd227b8d8e7dc
Fredrik von Post <fredrik.von.post@campusnykoping.se>
2024-10-13 14:35:20 +0200

    Filmlistan: Used Properties and an overridden ToString()

---
+++ Filmlistan/Movie.cs
@@ -0,0 +1,15 @@
+﻿namespace Filmlistan;
+    public class Movie
+    {
+        public string Name { get; set; }
+        public string Director { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int LengthInMinutes { get; set; }
+        public bool SuitableForChildren { get; set; }
+        public string[] Stars { get; set; }
+    public override string ToString()
+    {
+        return $"{Name} är en film som regisserats av {Director} med premiär {ReleaseDate} och är {LengthInMinutes} minuter.\nSkådespelarna i filmen är {string.Join(",",Stars)}. Den är {(SuitableForChildren? "" : "inte")} lämplig för barn.";
+    }
+    }
+
---
+++ Filmlistan/Program.cs
@@ -0,0 +1,38 @@
+﻿using Filmlistan;
+
+Movie movie1 = new Movie
+{
+    Name = "The Lion King",
+    Director = "Roger Allers, Rob Minkoff",
+    ReleaseDate = new DateTime(1994, 6, 24),
+    LengthInMinutes = 88,
+    SuitableForChildren = true,
+    Stars = new string[] { "Matthew Broderick", "James Earl Jones", "Jeremy Irons" }
+};
+
+Movie movie2 = new Movie
+{
+    Name = "Inception",
+    Director = "Christopher Nolan",
+    ReleaseDate = new DateTime(2010, 7, 16),
+    LengthInMinutes = 148,
+    SuitableForChildren = false,
+    Stars = new string[] { "Leonardo DiCaprio", "Joseph Gordon-Levitt", "Ellen Page" }
+};
+
+Movie movie3 = new Movie
+{
+    Name = "The Matrix",
+    Director = "Lana Wachowski, Lilly Wachowski",
+    ReleaseDate = new DateTime(1999, 3, 31),
+    LengthInMinutes = 136,
+    SuitableForChildren = false,
+    Stars = new string[] { "Keanu Reeves", "Laurence Fishburne", "Carrie-Anne Moss" }
+};
+
+List<Movie> movies = new(){movie1, movie2, movie3};
+foreach (var movie in movies)
+{
+    Console.WriteLine(movie);
+    Console.WriteLine();
+}
