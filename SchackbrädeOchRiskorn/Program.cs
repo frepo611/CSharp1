873ef01ae61bc1e30d87b191cba118ea4beb9c35
Fredrik von Post <fredrik.von.post@campusnykoping.se>
2024-10-13 13:29:51 +0200

    Riskorn: Showing output in scientific notation. Using ulong

---
+++ SchackbrädeOchRiskorn/Program.cs
@@ -0,0 +1,26 @@
+﻿ulong[,] chessboard = new ulong[8,8];
+ulong grains = 1;
+const ulong SWEDISHPOPULATION = 10_000_000;
+const ulong GOOGLEWORTH = 1_340_000_000_000;
+bool hasHitPopulation = false;
+bool hasHitGoogleGrowth = false;
+
+
+for (int i = 0; i < 8; i++)
+{
+    for (int j = 0; j < 8; j++)
+    {
+        chessboard[i, j] = grains;
+        if (grains > SWEDISHPOPULATION && hasHitPopulation==false)
+        {
+            Console.WriteLine($"({i}+{j}={i+j}): {grains:E2}");
+            hasHitPopulation = true;
+        }
+        if (grains > GOOGLEWORTH && hasHitGoogleGrowth==false)
+        {
+            Console.WriteLine($"({i}+{j}={i + j}): {grains:E2}");
+            hasHitGoogleGrowth = true;
+        }
+        grains = 2 * grains;
+    }
+};
