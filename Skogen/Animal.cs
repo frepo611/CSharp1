59df32d73e0684c229f897074a18703d56fd38dd
Fredrik von Post <fredrik.von.post@campusnykoping.se>
2024-10-16 15:24:48 +0200

    Skogen: Feature complete

---
+++ Skogen/Animal.cs
@@ -0,0 +1,58 @@
+﻿namespace Skogen;
+internal class Animal
+{
+    public Species Name { get; set; }
+    bool IsNoctural { get; init; }
+    string Movement { get; init; }
+
+    public Animal(Species name)
+    {
+        Name = name;
+        switch (name)
+        {
+            case Species.Vargen:
+                IsNoctural = true;
+                Movement = "jagar envist sitt byte.";
+                break;
+            case Species.Fladdermusen:
+                IsNoctural = true;
+                Movement = "flyger runt bland träden i jakt på mat.";
+                break;
+            case Species.Delfinen:
+                IsNoctural = false;
+                Movement = "simmar i floden och letar fisk.";
+                break;
+            case Species.Ugglan:
+                IsNoctural = true;
+                Movement = "spanar på marken efter byten.";
+                break;
+            case Species.Hästen:
+                IsNoctural = false;
+                Movement = "betar gräs.";
+                break;
+            default:
+                break;
+        }
+    }
+    public void Activate(bool isNight)
+    {
+        string activity;
+        if (isNight == IsNoctural)
+        {
+            activity = $"{Name} {Movement}";
+        }
+        else
+        {
+            activity = $"{Name} sover.";            ;
+        }
+        Console.WriteLine(activity);
+    }
+    public enum Species
+    {
+        Vargen,
+        Fladdermusen,
+        Delfinen,
+        Ugglan,
+        Hästen
+    }
+}
---
+++ Skogen/Program.cs
@@ -0,0 +1,66 @@
+﻿using Skogen;
+using System.Net.Quic;
+using static Skogen.Animal;
+
+Animal wolf1 = new Animal(Species.Vargen);
+Animal wolf2 = new Animal(Species.Vargen);
+Animal bat1 = new Animal(Species.Fladdermusen);
+Animal doplhin = new Animal(Species.Delfinen);
+Animal eagle = new Animal(Species.Ugglan);
+Animal horse = new Animal(Species.Hästen);
+
+
+List<Animal> forest = new List<Animal>();
+forest.Add(wolf1);
+forest.Add(wolf2);
+forest.Add(bat1);
+forest.Add(doplhin);
+forest.Add(eagle);
+forest.Add(horse);
+
+bool isNight = false;
+bool loopAgain = true;
+
+
+do
+{
+    Console.WriteLine("Vad gör djuren?\n");
+    Console.WriteLine("Är det dag eller natt i skogen? (D/N)?");
+    bool validKey = false;
+    do
+    {
+        var pressedKey = Console.ReadKey(true);
+        switch (pressedKey.Key)
+        {
+            case ConsoleKey.D:
+                isNight = false;
+                validKey = true;
+                break;
+            case ConsoleKey.N:
+                isNight = true;
+                validKey = true;
+                break;
+            default:
+                break;
+        }
+    } while (validKey == false);
+    Console.WriteLine();
+    Console.WriteLine($"Det är {(isNight ? "natt" : "dag")} i skogen.");
+    foreach (var animal in forest)
+    {
+        animal.Activate(isNight);
+    }
+    Console.WriteLine();
+    Console.WriteLine("Tryck Q för att avsluta");
+    var pressedKey1 = Console.ReadKey(true);
+    switch (pressedKey1.Key)
+    {
+        case ConsoleKey.Q:
+            loopAgain = false;
+            break;
+        default:
+            break;
+    }
+    Console.Clear();
+
+} while (loopAgain);
